2c67ecd0428b132f25ffdeacb5befd0cb01155fc
MrKirk1209 <k.godov@list.ru>
2025-04-14 10:40:40 +0300

    task2

--- main.py
+++ main.py
@@ -1,89 +1,129 @@
-from fastapi import FastAPI,HTTPException,Depends
-from database import get_db
-from sqlalchemy.orm import Session
-import models as m
-from typing import List
-import pyd
-app=FastAPI()
-
-@app.get("/movie", response_model=List[pyd.BaseMovie])
-def get_all_movie(db:Session=Depends(get_db)):
-    movies=db.query(m.Movie).all()
-    return movies
-
-@app.get("/product", response_model=List[pyd.ProductSchema])
-def get_all_products(db:Session=Depends(get_db)):
-    prods=db.query(m.Product).all()
-    return prods
-
-@app.get("/products/{product_id}")
-def get_product(product_id:int,db: Session=Depends(get_db)):
-    prod=db.query(m.Product).filter(
-        m.Product.id==product_id
-    ).first()
-
-    if not prod:
-        raise HTTPException(404,"НЕТ")
-    return prod
+from typing import Union
+import random, math
+from fastapi import FastAPI,HTTPException,Path
+from itertools import islice
+from typing import Optional
+app = FastAPI()
 
 
-@app.get("/movie/{movie_id}")
-def get_movie(movie_id:int,db: Session=Depends(get_db)):
-    mov=db.query(m.Movie).filter(
-        m.Movie.id==movie_id
-    ).first()
 
-    if not mov:
-        raise HTTPException(404,"НЕТ")
-    return mov
+items=[{
+    "id":1,
+    "name":"Toster",
+    "price":10,
+    "description":"this is toster",
+},
+{
+    "id":2,
+    "name":"laptop",
+    "price":100,
+    "description":"laptop?",
+},
+{
+    "id":3,
+    "name":"pc",
+    "price":200,
+    "description":"pc is power",
+},
+{
+        "id": 4,
+        "name": "Smartphone",
+        "price": 300,
+        "description": "Latest model with great camera"
+    },
+    {
+        "id": 5,
+        "name": "Headphones",
+        "price": 50,
+        "description": "Noise-cancelling wireless headphones"
+    },
+    {
+        "id": 6,
+        "name": "Keyboard",
+        "price": 25,
+        "description": "Mechanical keyboard for gamers"
+    },
+    {
+        "id": 7,
+        "name": "Monitor",
+        "price":150,
+        "description": "24-inch Full HD display"
+    },
+    {
+        "id": 8,
+        "name": "Mouse",
+        "price": 15,
+        "description": "Ergonomic wireless mouse"
+    },
+    {
+        "id": 9,
+        "name": "Tablet",
+        "price": 120,
+        "description": "Portable and lightweight"
+    },
+    {
+        "id": 10,
+        "name": "Smartwatch",
+        "price": 80,
+        "description": "Tracks fitness and notifications"
+    }
+]
 
-@app.post("/product")
-def create_product(product:pyd.CreateProduct,db: Session= Depends(get_db)):
-    product_db=db.query(m.Product).filter(m.Product.name==product.name).first()
-    if product_db:
-        raise HTTPException(400,"Есть")
-    product_db=m.Product(name=product.name)
+@app.get("/items/")
+def search_item(name: str | None = None, min_price: int | None = None ,max_price: int | None = None,limit: int | None = None):
+    def filter_items():
+        for i in items:
+            if name is not None and i["name"]!=name:
+                continue
+            if min_price is not None and i["price"]<= min_price:
+                continue 
+            if max_price is not None and i["price"]>=max_price:
+                continue
+            yield i
+    filtered=filter_items()
+    if limit is not None and limit>0:
+        filtered=islice(filtered, limit)
+    res=list(filtered)
+    if not res:
+        raise HTTPException(status_code=404, detail="Items not found")
     
+    return res    
+        
+@app.get("/items/{item_id}")
+def read_item_one(item_id: int = Path(..., gt=0)):
+    for i in items:
+        if i["id"]==item_id:
+            return i    
+    raise HTTPException(status_code=404, detail="Item not found")
 
-    db.add(product_db)
-    db.commit()
-    return product_db
+@app.post("/items/")
+def create_item(name: str,price: Optional[int] = None, description: Optional[str] = None):
 
+    if len(name) < 3 or len(name) > 100:
+        raise HTTPException(
+            status_code=400,
+            detail="Название должно быть больше двух символов или меньше ста символов"
+        )
+    if price <= 0:
+        raise HTTPException(
+            status_code=400,
+            detail="Цена должна быть больше нуля"
+        )
+    
+    if description is not None and len(description) > 500:
+        raise HTTPException(
+            status_code=400,
+            detail="Описание не должно быть больше 500 символов"
+        )
 
-@app.post("/movie")
-def create_movie(movie:pyd.CreateMovie,db: Session= Depends(get_db)):
-    movie_db=db.query(m.Movie).filter(m.Movie.name==movie.name).first()
-    if movie_db:
-        raise HTTPException(400,"Есть")
-    movie_db=m.Movie(name=movie.name,genre=movie.genre)
-
-    db.add(movie_db)
-    db.commit()
-    return movie_db
-
-
-@app.delete("/products/{product_id}")
-def d_product(product_id:int,db: Session=Depends(get_db)):
-    prod=db.query(m.Product).filter(
-        m.Product.id==product_id
-    ).first()
-
-    if not prod:
-        raise HTTPException(404,"НЕТ")
-    db.delete(prod)
-    db.commit()
-    return {"msg":"Товар удален"}
-
-@app.delete("/movies/{movie_id}")
-def d_movie(movie_id:int,db: Session=Depends(get_db)):
-    prod=db.query(m.Movie).filter(
-        m.Movie.id==movie_id
-    ).first()
-
-    if not prod:
-        raise HTTPException(404,"НЕТ")
-    db.delete(prod)
-    db.commit()
-    return {"msg":"Товар удален"}
-#создать модель с 3 полями или больше, сделать посев, и метод
-# для вывода всех строк из вашей модели
+    new_id = items[-1]["id"] + 1 if items else 1
+    
+    new_item = {
+        "id": new_id,
+        "name":name,
+        "price":price,
+        "description":description
+    }
+    items.append(new_item)
+    
+    return {"status": "success", "item": new_item}
